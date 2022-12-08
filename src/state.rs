7d26c49d09f27c30253b47d0c48fe35b4a69e283
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-12-08 14:23:32 +0000

    state.rs

--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,41 @@
+//storing contract data 
+use std::process::ExitStatus;
+
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map}; // we should import Map 
+
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+pub struct Config {
+    pub owner: Addr, //address stored here
 }
-
-pub const STATE: Item<State> = Item::new("state");
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Entry { //to do 
+    pub id: u64,
+    pub description: String, 
+    pub status: Status, 
+    pub priority: Priority,
+    
+}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub enum Status{
+    ToDo,
+    InProgress,
+    Done,
+    Cancelled
+}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub enum Priority{
+    None,
+    Low,
+    Medium,
+    High
+}
+//creating constant to store varieble on the chain
+//item stores (as a single varieble) by storage key
+pub const CONFIG: Item<Config> = Item::new("config");
+pub const ENTRY_SEQ: Item<u64> = Item::new("entry_seq"); //entry stored as entry_seq 
+pub const LIST: Map<u64, Entry> = Map::new("list"); //it's a map with a key: id and value: entry
